12f02ea67a7d3e2bfbf56010f6b727d7b77e0158
Peter Osusky <peterosusky94@gmail.com>
2023-11-01 11:28:47 +0100

    refactored imageScrollView

--- MapApp/View/DetailView/PlaceDetailView.swift
+++ MapApp/View/DetailView/PlaceDetailView.swift
@@ -17,18 +17,7 @@ struct PlaceDetailsView: View {
             VStack(alignment: .leading, spacing: 15) {
                 PlaceDetailHeaderView(place: place)
                 PlaceDetailLocationView(place: place)
-                
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack(spacing: 10) {
-                        ForEach(viewModel.photos, id: \.id) { photo in
-                            URLImageView(withURL: photo.fullURL)
-                                .frame(width: 150, height: 150)
-                                .cornerRadius(10)
-                        }
-                    }
-                    .padding(.top, 10)
-                }
-                
+                PlaceDetailImageScrollView(viewModel: viewModel)                
                 PlaceDetailGeocodeView(place: place)
             }
             .padding()
---
+++ MapApp/View/DetailView/SubViews/PlaceDetailImageView.swift
@@ -0,0 +1,48 @@
+//
+//  PlaceDetailImageScrollView.swift
+//  MapApp
+//
+//  Created by Peter on 01/11/2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct PlaceDetailImageScrollView: View {
+    @ObservedObject var viewModel: PlaceDetailViewModel
+
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 10) {
+                ForEach(viewModel.photos, id: \.id) { photo in
+                    ImageView(url: photo.fullURL)
+                        .frame(width: 150, height: 150)
+                        .cornerRadius(10)
+                }
+            }
+            .padding(.top, 10)
+        }
+    }
+}
+
+struct ImageView: View {
+    let url: URL
+
+    var body: some View {
+        AsyncImage(url: url, transaction: Transaction(animation: .easeInOut)) { phase in
+            switch phase {
+            case .empty:
+                Image(systemName: "photo")
+                    .resizable()
+            case .success(let image):
+                image.resizable()
+            case .failure:
+                Image(systemName: "exclamationmark.triangle")
+                    .resizable()
+            @unknown default:
+                EmptyView()
+            }
+        }
+    }
+}
+
